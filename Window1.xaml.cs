bb4022ea640acffa4de5bdb3df9d1775b840c90c
u31504 <u31504@P31504>
2022-11-12 10:45:49 +1000

    пфтаюсь что-то сделать, но что-то не выходит

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -10,26 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Wpf_ISRPO_PR6
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
 
-        private void Data_Click(object sender, RoutedEventArgs e)
+        private void Podschitst_Click(object sender, RoutedEventArgs e)
         {
-            Window1 window1 = new Window1();
-            window1.Show();
-            this.Close();
+            
         }
     }
 }
